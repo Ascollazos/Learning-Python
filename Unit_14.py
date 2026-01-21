d294094178d224a5e23c2a719d40ecdb19907f87
Ascollazos <asafdavidcollazosmachacon@gmail.com>
2026-01-21 17:15:46 -0500

    Actualizacion Unit_14

--- Unit_14.py
+++ Unit_14.py
@@ -7,5 +7,17 @@ elif numero == 3:
     numeroTexto = "Numero tres"
 else:
     numeroTexto = "Valor fuera de rango"
-
 print(f'El numero proporcionado: {numero} - {numeroTexto}') 
+
+
+def evaluar_dia(dia):
+    match dia:
+        case "lunes":
+            print("Es lunes, ¡a trabajar!")
+        case "viernes":
+            print("¡Casi es fin de semana!")
+        case _: # El caso por defecto (similar a 'else' o 'default')
+            print("Es otro día de la semana.")
+
+evaluar_dia("lunes")
+evaluar_dia("domingo")
